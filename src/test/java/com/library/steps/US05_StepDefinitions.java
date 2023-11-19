15a1a2b6a7664ae63a126fa742efe6e8a498110b
daryamatseyuk <matdasha95@gmail.com>
2023-11-19 14:03:51 -0500

    Updated Step Defs

---
+++ src/test/java/com/library/steps/US05_StepDefinitions.java
@@ -0,0 +1,22 @@
+package com.library.steps;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class US05_StepDefinitions {
+
+    @When("I execute query to find most popular book genre")
+    public void iExecuteQueryToFindMostPopularBookGenre() {
+
+
+
+    }
+
+    @Then("verify {string} is the most popular book genre.")
+    public void verifyIsTheMostPopularBookGenre(String arg0) {
+
+
+
+    }
+
+}
