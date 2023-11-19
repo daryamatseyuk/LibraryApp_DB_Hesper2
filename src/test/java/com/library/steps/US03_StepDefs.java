43cf965ddd45d6ca89036b1b3271988833ef4def
JAKUDA <j_daotra@yahoo.com>
2023-11-19 15:56:01 -0500

    US03 finished and passed

--- src/test/java/com/library/steps/US03_StepDefs.java
+++ src/test/java/com/library/steps/US03_StepDefs.java
@@ -3,8 +3,10 @@ package com.library.steps;
 import com.library.pages.BasePage;
 import com.library.pages.BookPage;
 import com.library.utility.BrowserUtil;
+import com.library.utility.DB_Util;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.Select;
@@ -15,7 +17,6 @@ import java.util.stream.Collectors;
 
 public class US03_StepDefs {
     BookPage bookPage = new BookPage();
-    Select select = new Select(bookPage.categoryDropdown);
 
     @When("the user navigates to {string} page")
     public void the_user_navigates_to_page(String module) {
@@ -24,13 +25,24 @@ public class US03_StepDefs {
     @When("the user clicks book categories")
     public void the_user_clicks_book_categories() {
         bookPage.categoryDropdown.click();
-        BrowserUtil.waitFor(3);
     }
     @Then("verify book categories must match book_categories table from db")
     public void verify_book_categories_must_match_book_categories_table_from_db() {
-        List<WebElement> BookCategories = select.getOptions();
-        List<String> actualBookCategories = BookCategories.stream().map(WebElement::getText).collect(Collectors.toList());
+
+        Select select = new Select(bookPage.categoryDropdown);
+        List<WebElement> bookCategories = select.getOptions();
+        List<String> actualBookCategories = bookCategories.stream().map(WebElement::getText).collect(Collectors.toList());
+        actualBookCategories.remove(0);
+
         System.out.println(actualBookCategories);
+
+        DB_Util.runQuery("select name from book_categories");
+        List<String> expectedColumnDataAsList = DB_Util.getColumnDataAsList(1);
+
+        System.out.println(expectedColumnDataAsList);
+
+       Assert.assertEquals(expectedColumnDataAsList,actualBookCategories);
+
     }
 
 }
