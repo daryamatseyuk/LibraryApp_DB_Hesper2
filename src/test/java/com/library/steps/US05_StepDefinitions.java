4441968ed1ff6662f07bb6dd29b7ca9a32a80c71
yotgon <y.otgon@gmail.com>
2023-11-27 13:56:46 -0500

    added my US05

--- src/test/java/com/library/steps/US05_StepDefinitions.java
+++ src/test/java/com/library/steps/US05_StepDefinitions.java
@@ -29,4 +29,20 @@ public class US05_StepDefinitions {
 
     }
 
+
+    @When("I execute query to find most popular book genre. YO")
+    public void i_execute_query_to_find_most_popular_book_genre_YO() {
+        DB_Util.runQuery("select bc.name,count(*) from book_borrow bb\n" +
+                "inner join books b on bb.book_id = b.id\n" +
+                "inner join book_categories bc on b.book_category_id=bc.id\n" +
+                "group by name\n" +
+                "order by 2 desc");
+    }
+
+    @Then("verify {string} is the most popular book genre. YO")
+    public void verify_is_the_most_popular_book_genre_YO(String expectedMostPopularGenre) {
+        String actualMostPopularGenre = DB_Util.getFirstRowFirstColumn();
+        System.out.println(actualMostPopularGenre);
+        Assert.assertEquals(expectedMostPopularGenre,actualMostPopularGenre);
+    }
 }
